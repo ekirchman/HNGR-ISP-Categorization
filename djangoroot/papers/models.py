5492cfe4587eed1e80e6af200a00189527225576
ekirchman <47666987+ekirchman@users.noreply.github.com>
2024-11-09 11:04:25 -0600

    Created db. Created admin page under /admin

--- djangoroot/papers/models.py
+++ djangoroot/papers/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Document(models.Model):
+    language = models.CharField(max_length=200)
+    major = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    fname = models.CharField(max_length=200)
+    lname = models.CharField(max_length=200)
+    f_path = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.f_path
+
+
+class Doc_theme(models.Model):
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    theme_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.theme_name
+
